5a32a8e1939653c50ac5316e69abc72b9f1b51c8
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2017-07-04 21:08:08 +0000

    Added initial track, cleaning up some styling, basic templating and state manager

--- js/main.js
+++ js/main.js
@@ -1,22 +1,13 @@
-const jayZdataPromise = $.when($.getJSON("/requests/jayz-1.json"));
-
-jayZdataPromise.then(
-  jayZdata =>
-    (document.querySelector("#response").innerHTML = prettyjson.render(
-      jayZdata
-    ))
-);
-
-function Song(render, state) {
+function SongTemplate(render, state) {
   render`
   <div class="Song">
     <div class="Song__cover">
       <img class="Cover__image" src="${state.img}" />
     </div>
     <div class="Song__metadata">
-      <h3 class="Song__title">Song title <small>Album title</small></h3>
+      <h3 class="Song__title">${state.title}<small> ${state.album}</small></h3>
       <div class="Song__frequency-chart"></div>
-      <div class="Song__player-progress"></div>
+      <progress value="${state.progress}" max="30" class="Song__player-progress"></progress>
       <div class="Song__time-progress">
         <span class="Time-Progress__seconds Time-Progress__seconds--is-elapsed">0:00</span>
         <span class="Time-Progress__seconds Time-Progress__seconds--is-remaining">0:00</span>
@@ -31,9 +22,34 @@ function Song(render, state) {
   `;
 }
 
-Song(
-  hyperHTML.bind(document.getElementById('song-root')),
-  {
-    img: '/requests/100x100bb.jpg'
+class Track {
+  constructor(img, title, album) {
+    this.img = img;
+    this.title = title;
+    this.album = album;
+    this.progress = 0;
+    this.progressInterval = setInterval(this.updateProgress.bind(this), 1000)
   }
-)
+  
+  updateProgress() {
+    this.progress = ~~(this.progress + 1);
+  }
+}
+
+const renderNode = hyperHTML.bind(document.getElementById('song-root'))
+const jayZdataPromise = $.when($.getJSON("/requests/jayz-1.json"));
+
+jayZdataPromise.then(
+  jayZdata => {
+    (document.querySelector("#response").innerHTML = prettyjson.render(
+      jayZdata
+    ))
+    let track = jayZdata.results[0]
+    let progress = 0
+    let trackInstance = new Track(track.artworkUrl100, track.trackName, track.collectionName)
+    SongTemplate(renderNode, trackInstance)
+    setInterval(SongTemplate, 1000, renderNode, trackInstance);
+  }
+);
+
+
