d4bc1157f61630dc36de5afb9099691ca362dad4
Jose Jesus Perez Aguinaga <jj.perez.aguinaga@gmail.com>
2017-07-06 13:29:09 +0000

    Added audio with actual syncing on progress bar and control manipulation for pause and rewind

--- js/main.js
+++ js/main.js
@@ -7,15 +7,15 @@ function SongTemplate(render, state) {
     <div class="Song__metadata">
       <h3 class="Song__title">${state.title}<small> ${state.album}</small></h3>
       <div class="Song__frequency-chart"></div>
-      <progress value="${state.progress}" max="30" class="Song__player-progress"></progress>
+      <progress value="${state.audio.currentTime}" max="${state.max}" class="Song__player-progress"></progress>
       <div class="Song__time-progress">
-        <span class="Time-Progress__seconds Time-Progress__seconds--is-elapsed">0:00</span>
-        <span class="Time-Progress__seconds Time-Progress__seconds--is-remaining">0:00</span>
+        <span class="Time-Progress__seconds Time-Progress__seconds--is-elapsed">0:${state.elapsedSeconds}</span>
+        <span class="Time-Progress__seconds Time-Progress__seconds--is-remaining">0:${state.remainingSeconds}</span>
       </div>
       <div class="Song__controls">
-        <a role="button" class="Control__button Control__button--is-backwards"></a>
-        <a role="button" class="Control__button Control__button--is-play"></a>
-        <a role="button" class="Control__button Control__button--is-forward"></a>
+        <a role="button" onClick="${state.rewind.bind(state)}" class="Control__button Control__button--is-backwards">&#x23ea;</a>
+        <a role="button" onClick="${state.toggle.bind(state)}" class="Control__button Control__button--is-play">&#x23ef;</a>
+        <a role="button" class="Control__button Control__button--is-forward">&#x23e9;</a>
       </div>
     </div>
   </div>
@@ -23,16 +23,46 @@ function SongTemplate(render, state) {
 }
 
 class Track {
-  constructor(img, title, album) {
+  constructor(img, title, album, trackTimeMillis, audio) {
     this.img = img;
     this.title = title;
     this.album = album;
-    this.progress = 0;
-    this.progressInterval = setInterval(this.updateProgress.bind(this), 1000)
+    this.audio = audio
+    //this.max = trackTimeMillis / 10000
+    this.max = 30
+    
+    this.play();
   }
   
-  updateProgress() {
-    this.progress = ~~(this.progress + 1);
+  toggle() {
+    this._isPlaying ? this.stop() : this.play()
+  }
+  
+  rewind() {
+    this.audio.currentTime = 0
+  }
+  
+  stop() {
+    this._isPlaying = false
+    this.audio.pause();
+  }
+  
+  play() {
+    this._isPlaying = true
+    this.audio.play();
+  }
+  
+  _formatSeconds(value) {
+    const roundedValue = Math.floor(value)
+    return roundedValue >= 10 ? roundedValue : `0${roundedValue}`
+  }
+  
+  get elapsedSeconds() {
+    return this._formatSeconds(this.audio.currentTime)
+  }
+  
+  get remainingSeconds() {
+    return this._formatSeconds(this.max - this.audio.currentTime);
   }
 }
 
@@ -44,11 +74,28 @@ jayZdataPromise.then(
     (document.querySelector("#response").innerHTML = prettyjson.render(
       jayZdata
     ))
-    let track = jayZdata.results[0]
-    let progress = 0
-    let trackInstance = new Track(track.artworkUrl100, track.trackName, track.collectionName)
+    
+    const context = new AudioContext();
+    const analyser = context.createAnalyser();
+    const audio = new Audio();
+    
+    audio.crossOrigin = "anonymous";
+    audio.controls = true;
+    audio.autoplay = true;
+    audio.loop = true;
+    
+    const source = context.createMediaElementSource(audio);
+    source.connect(analyser);
+    analyser.connect(context.destination);
+    
+    let track = jayZdata.results[Math.floor(Math.random()*49)]
+    
+    audio.src = track.previewUrl;
+    
+    let trackInstance = new Track(track.artworkUrl100, track.trackName, track.collectionName, track.trackTimeMillis, audio)
     SongTemplate(renderNode, trackInstance)
-    setInterval(SongTemplate, 1000, renderNode, trackInstance);
+    
+    setInterval(SongTemplate, 0, renderNode, trackInstance);
   }
 );
 
